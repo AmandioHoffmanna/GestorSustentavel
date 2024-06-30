4042ae8fc980f3be46b0ca7b8c9bc2b137d0a6a7
Vinícius <vinicius.veiga@unidavi.edu.br>
2024-06-30 13:13:18 -0300

    Correções e melhorias no controlador EstoquesController

---
+++ database/migrations/2024_06_30_155538_update_usuario_id_in_estoques_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateUsuarioIdInEstoquesTable extends Migration
+{
+    public function up()
+    {
+        Schema::table('estoques', function (Blueprint $table) {
+            $table->unsignedBigInteger('usuario_id')->nullable()->change();
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('estoques', function (Blueprint $table) {
+            $table->unsignedBigInteger('usuario_id')->nullable(false)->change();
+        });
+    }
+}
